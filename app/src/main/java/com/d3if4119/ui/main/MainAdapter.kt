959f8411b12daf9dc53c6ae317fbfc7bf4d81976
Fauzian Pradana Putra <rynzler07@gmail.com>
2021-05-03 21:54:32 +0700

    Menampilkan gambar dari internet

--- app/src/main/java/com/d3if4119/ui/main/MainAdapter.kt
+++ app/src/main/java/com/d3if4119/ui/main/MainAdapter.kt
@@ -3,8 +3,11 @@ package com.d3if4119.ui.main
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.d3if4119.galerihewan.Hewan
+import com.d3if4119.galerihewan.R
 import com.d3if4119.galerihewan.databinding.ListItemBinding
+import com.d3if4119.network.HewanApi
 
 class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
     private val data = mutableListOf<Hewan>()
@@ -17,10 +20,15 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
 
     class ViewHolder(private val binding: ListItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(hewan: Hewan) = with(binding) {
-            namaTextView.text = hewan.nama
-            latinTextView.text = hewan.namaLatin
-            imageView.setImageResource(hewan.imageResId)
+        fun bind(hewan: Hewan) {
+            with(binding) {
+                namaTextView.text = hewan.nama
+                latinTextView.text = hewan.namaLatin
+                Glide.with(imageView.context)
+                    .load(HewanApi.getHewanUrl(hewan.imageId))
+                    .error(R.drawable.ic_baseline_broken_image_24)
+                    .into(imageView)
+            }
         }
     }
 
